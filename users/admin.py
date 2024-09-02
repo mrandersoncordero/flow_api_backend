d3f29196bf339deb2b783c393722f5fc01685681
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-02 16:18:35 -0400

    personalizar admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
+"""Tasks admin."""
+
+# Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'created']
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ['-created']
+
+
+admin.site.register(User, UserAdmin)
