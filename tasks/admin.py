12c1ce4c55269b089d84a638d84ccd85479f1e45
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-02 21:02:01 -0400

    task admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,50 @@
+"""Task admin."""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from .models import Department, TaskStatus, Task
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created']
+    search_fields = ['name']
+    ordering = ['-created']
+
+
+@admin.register(TaskStatus)
+class TaskStatuAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    list_editable = ['name']
+    ordering = ['pk']
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user__first_name',
+                    'department', 'status', 'hours', 'created']
+    search_fields = ['username', 'user__first_name',
+                     'user__last_name', 'title', 'status']
+    ordering = ['status', '-created']
+
+    raw_id_fields = ['user']
+
+    fieldsets = (
+        (None, {
+            "fields": (
+                ('title'),
+                ('description'),
+                ('user', 'department'),
+                ('status', 'hours')
+            ),
+        }),
+        ('Metadata', {
+            "fields": (
+                ('created', 'modified')
+            )
+        }),
+    )
+
+    readonly_fields = ('created', 'modified')
