f8f6283fc834b9c21f93679ff5b87021ecb319f0
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-03 14:10:38 -0400

    crear y mostrar usando el serializer

--- tasks/views/tasks.py
+++ tasks/views/tasks.py
@@ -8,54 +8,24 @@ from rest_framework.response import Response
 from tasks.models import Task, Department, TaskStatus
 from users.models import User
 
+# Serializers
+from tasks.serializers import TaskSerializer
+
+
 @api_view(["GET"])
 def list_tasks(request):
     """List circles."""
     # import ipdb; ipdb.set_trace()
-
     tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
 
-    data = []
-    for task in tasks:
-        data.append(
-            {
-                "user": task.user.username,  # Convertir a nombre de usuario
-                "title": task.title,
-                "description": task.description,
-                "department": task.department.name,  # Convertir a nombre del departamento
-                "status": task.status.name,  # Convertir a nombre del estado
-                "hours": str(task.hours),  # Convertir a cadena de texto (HH:MM:SS)
-            }
-        )
-
-    return Response(data)
+    return Response(serializer.data)
 
 
 @api_view(["POST"])
 def create_task(request):
     """Create task"""
-    user = User.objects.get(username=request.data["user"])
-    title = request.data["title"]
-    description = request.data["description"]
-    department = Department.objects.get(pk=request.data["department"])
-    status = TaskStatus.objects.get(pk=request.data["status"])
-    hours = request.data["hours"]
-
-    task = Task.objects.create(
-        user=user,
-        title=title,
-        description=description,
-        department=department,
-        status=status,
-        hours=hours,
-    )
-
-    data = {
-        "user": task.user.username,  # Convertir a nombre de usuario
-        "title": task.title,
-        "description": task.description,
-        "department": task.department.name,  # Convertir a nombre del departamento
-        "status": task.status.name,  # Convertir a nombre del estado
-        "hours": str(task.hours),  # Convertir a cadena de texto (HH:MM:SS)
-    }
-    return Response(data)
+    serializer = TaskSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    task = serializer.save()
+    return Response(TaskSerializer(task).data)
