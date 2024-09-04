d4a9b2e22d9dfd76adeb82c8cc4d0a0b733ba708
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-04 15:40:28 -0400

    urls add department

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,14 +1,21 @@
 """Taks URLs."""
 
 # Django
-from django.urls import path
+from django.urls import path, include
+
+# Django REST Framework
+from rest_framework.routers import DefaultRouter
 
 # Views
-from .views import list_tasks, create_task
+from .views import list_tasks, create_task, DepartmentViewSet
 
 app_name = 'tasks'
+
+router = DefaultRouter()
+router.register(r'', DepartmentViewSet, basename='department')
 
 urlpatterns = [
     path('tasks/', list_tasks),
     path('tasks/create/', create_task),
+    path('departments/', include(router.urls)),
 ]
