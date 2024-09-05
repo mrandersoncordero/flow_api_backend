4762e0a470b292ffdeb79730fd655d45010b841d
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-05 13:39:21 -0400

    urls users get

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@
 from django.urls import path
 
 # Views
-from .views import UserLoginAPIView, UserSingUpAPIView, AccountVerificationAPIView
+from .views import UserLoginAPIView, UserSingUpAPIView, AccountVerificationAPIView, UserAPIView
 
 app_name = "users"
 
@@ -12,4 +12,7 @@ urlpatterns = [
     path("users/login/", UserLoginAPIView.as_view(), name="login"),
     path("users/singup/", UserSingUpAPIView.as_view(), name="signup"),
     path("users/verify/", AccountVerificationAPIView.as_view(), name="verify"),
+    
+    path('users/', UserAPIView.as_view(), name='list'),
+    path('users/<int:pk>', UserAPIView.as_view()),
 ]
