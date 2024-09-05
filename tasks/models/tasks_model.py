b15c433db85dbff8e7493577e6220c68a5cf3eff
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-05 13:06:21 -0400

    nuevos campos para el modelo task

--- tasks/models/tasks_model.py
+++ tasks/models/tasks_model.py
@@ -9,12 +9,13 @@ from django.utils.timezone import datetime
 from flow.utils import FlowModels
 
 # Models
-from .department_model import Department
+from tasks.models import Department, Company
 
 
 class TaskStatus(models.Model):
     """TaskStatus model."""
-    name = models.CharField(max_length=50, unique=True, verbose_name='nombre')
+
+    name = models.CharField(max_length=50, unique=True, verbose_name="nombre")
 
     def __str__(self) -> str:
         return f"{self.name}"
@@ -22,21 +23,19 @@ class TaskStatus(models.Model):
 
 class Task(FlowModels, models.Model):
     """Tasks model."""
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.PROTECT)
-    title = models.CharField(max_length=200,
-                             unique=True,
-                             verbose_name='titulo')
-    description = models.TextField(max_length=500,
-                                   null=True,
-                                   blank=True,
-                                   verbose_name='descripcion')
-    department = models.ForeignKey(Department,
-                                   on_delete=models.PROTECT)
-    status = models.ForeignKey(TaskStatus,
-                               on_delete=models.SET_DEFAULT,
-                               default=1)
-    hours = models.TimeField(verbose_name='tiempo')
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    title = models.CharField(max_length=200, unique=True, verbose_name="titulo")
+    description = models.TextField(
+        max_length=500, null=True, blank=True, verbose_name="descripcion"
+    )
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT, default=1)
+    status = models.ForeignKey(TaskStatus, on_delete=models.SET_NULL, null=True)
+    hours = models.TimeField(verbose_name="tiempo")
+
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self) -> str:
-        return f'{self.title} - by {self.user.username}'
+        return f"{self.title} - by {self.user.username}"
