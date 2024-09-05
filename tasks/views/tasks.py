3d28a30cae2c720098c4edcb3d4214f3e526079b
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-05 10:21:48 -0400

    usar viewsets y cbv

--- tasks/views/tasks.py
+++ tasks/views/tasks.py
@@ -1,31 +1,65 @@
 """Tasks views."""
 
+# Django
+from django.shortcuts import get_object_or_404
+
 # Django REST Framework
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
 
 # Models
-from tasks.models import Task, Department, TaskStatus
-from users.models import User
+from tasks.models import Task, TaskStatus
 
 # Serializers
-from tasks.serializers import TaskSerializer
+from tasks.serializers import TaskSerializer, TaskStatusModelSerializer
 
 
-@api_view(["GET"])
-def list_tasks(request):
-    """List circles."""
-    # import ipdb; ipdb.set_trace()
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
+class TaskStatusAPIView(APIView):
+    """Vista Basada en clases para manejar POST, GET, PUT, PATCH y DELETE."""
 
-    return Response(serializer.data)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, pk=None):
+        # Si se pasa un ID (pk) se retorna un status especifico
+        if pk:
+            task_status = get_object_or_404(TaskStatus, pk=pk)
+            serializer = TaskStatusModelSerializer(task_status)
+            return Response(serializer.data)
+
+        task_status = TaskStatus.objects.all()
+        serializer = TaskStatusModelSerializer(task_status, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        # Crear un nuevo status
+        serializer = TaskStatusModelSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        task = serializer.save()
+        return Response(
+            TaskStatusModelSerializer(task).data, status=status.HTTP_201_CREATED
+        )
+
+    def put(self, request, pk):
+        # Actualizar un status
+        task_status = get_object_or_404(TaskStatus, pk=pk)
+        serializer = TaskStatusModelSerializer(task_status, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        # Eliminar un status
+        task_status = get_object_or_404(TaskStatus, pk=pk)
+        task_status.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(["POST"])
-def create_task(request):
-    """Create task"""
-    serializer = TaskSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    task = serializer.save()
-    return Response(TaskSerializer(task).data)
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = (IsAuthenticated,)
