0aa427c35fb401f6e4097cdb14d891d551f9f38d
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-05 09:28:46 -0400

    permisison y get_query_set

--- tasks/views/department.py
+++ tasks/views/department.py
@@ -2,7 +2,7 @@
 
 # Django REST Framework
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticated
 # Models
 from tasks.models import Department
 
@@ -14,3 +14,18 @@ class DepartmentViewSet(viewsets.ModelViewSet):
 
     queryset = Department.objects.all()
     serializer_class = DeparmentSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        """Filtrar por parametros de la URL"""
+        queryset = Department.objects.all()
+        name = self.request.query_params.get('name', None)
+        description = self.request.query_params.get('description', None)
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if description:
+            queryset = queryset.filter(description__icontains=description)
+        
+        return queryset
+    
