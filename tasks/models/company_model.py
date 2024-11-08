062ebddb47c712b9340451df5f8bbc8f8c2c0714
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-11-08 11:53:27 -0400

    aumentar longitud de caracteres para company

--- tasks/models/company_model.py
+++ tasks/models/company_model.py
@@ -10,7 +10,7 @@ from flow.utils import FlowModels
 class Company(FlowModels):
     """Model Company."""
 
-    name = models.CharField(max_length=40, unique=True, verbose_name="nombre")
+    name = models.CharField(max_length=100, unique=True, verbose_name="nombre")
 
     def __str__(self) -> str:
         return str(self.name)
