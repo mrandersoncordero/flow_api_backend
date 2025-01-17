ed4f493cf8558eeedeb58d5bff0b3c1a99c50d6b
mrandersoncordero <acorderofigueroa7@gmail.com>
2025-01-17 14:57:07 -0400

    [FEAT] human resource model

---
+++ users/models/human_resources_model.py
@@ -0,0 +1,31 @@
+"""Human resources model."""
+
+# Django
+from django.db import models
+from django.conf import settings
+
+# Utilities
+from flow.utils.main_model import MainModel
+
+
+class HumanResource(MainModel, models.Model):
+    """HumanResource
+
+    Args:
+        MainModel (_type_): _description_
+        models (_type_): _description_
+    """
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, to_field="id"
+    )
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    picture = models.ImageField(upload_to="users/pictures", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    class Meta:
+        indexes = [models.Index(fields=["-created"])]
