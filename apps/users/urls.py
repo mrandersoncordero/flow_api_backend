61108508e674ec5001192e4a5d7645cee42ca2bf
mrandersoncordero <acorderofigueroa7@gmail.com>
2025-02-11 14:48:22 -0400

    [FIX] human resource urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,7 +4,14 @@
 from django.urls import path
 
 # Views
-from .views import UserLoginAPIView, UserSingUpAPIView, AccountVerificationAPIView, UserAPIView
+from .views import (
+    UserLoginAPIView,
+    UserSingUpAPIView,
+    AccountVerificationAPIView,
+    UserAPIView,
+    HumanResourceCreateAPIView,
+    HumanResourceDetailUpdateAPIView
+)
 
 app_name = "users"
 
@@ -12,7 +19,16 @@ urlpatterns = [
     path("users/login/", UserLoginAPIView.as_view(), name="login"),
     path("users/singup/", UserSingUpAPIView.as_view(), name="signup"),
     path("users/verify/", AccountVerificationAPIView.as_view(), name="verify"),
-    
-    path('users/', UserAPIView.as_view(), name='list'),
-    path('users/<int:pk>', UserAPIView.as_view()),
+    path("users/", UserAPIView.as_view(), name="list"),
+    path("users/<int:pk>", UserAPIView.as_view()),
+    path(
+        "human-resources/",
+        HumanResourceCreateAPIView.as_view(),
+        name="create-human-resource",
+    ),
+    path(
+        "human-resources/me/",
+        HumanResourceDetailUpdateAPIView.as_view(),
+        name="detail-update-human-resource",
+    ),
 ]
