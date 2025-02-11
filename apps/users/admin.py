40e4727e77664c05f3f75b04587bda5c7fbc713d
mrandersoncordero <acorderofigueroa7@gmail.com>
2025-02-11 11:24:06 -0400

    [FIX] is_verified edit in admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -11,7 +11,8 @@ from .models import User
 class UserAdmin(BaseUserAdmin):
     """Add profile admin to base user admin."""
 
-    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'created']
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_verified', 'created']
+    list_editable = ['is_verified']
     search_fields = ['email', 'first_name', 'last_name']
     ordering = ['-created']
 
