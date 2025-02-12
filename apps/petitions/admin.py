fdb3c2e4dcc297b0740a4f4b377909d6fb9abf83
mrandersoncordero <acorderofigueroa7@gmail.com>
2025-02-12 10:28:44 -0400

    [FIX] modificar admin petition

--- apps/petitions/admin.py
+++ apps/petitions/admin.py
@@ -4,39 +4,65 @@ from django.contrib import admin
 # Models
 from .models import Department, Petition
 
+
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name']
-    list_display_links = ['id']
-    list_editable = ['name']
+    list_display = ["id", "name"]
+    list_display_links = ["id"]
+    list_editable = ["name"]
 
     fieldsets = (
-        (None, {
-            "fields": (
-                'name', 'active'
-            ),
-        }),
-        ('Metadata', {
-            'fields': (
-                ('created', 'modified'),
-            )
-        })
+        (
+            None,
+            {
+                "fields": ("name", "active"),
+            },
+        ),
+        ("Metadata", {"fields": (("created", "modified"),)}),
     )
-    
-    readonly_fields = ('created', 'modified')
-    
+
+    readonly_fields = ("created", "modified")
+
 
 @admin.register(Petition)
 class PetitionAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'parent_petition', 'get_priority_display', 'get_status_approval_display']
-    list_filter = ['priority', 'status_approval']
-    search_fields = ['title', 'description']
-    autocomplete_fields = ['parent_petition']
+    list_display = [
+        "id",
+        "title",
+        "priority",
+        "is_main",
+        "status_approval",
+        "department"
+    ]
+    list_filter = ["priority", "status_approval", "created"]
+    search_fields = ["title", "description"]
 
-    def get_priority_display(self, obj):
-        return obj.get_priority_display()  # Devuelve el valor legible del ChoiceField
-    get_priority_display.short_description = "Priority"  # Nombre de la columna en Django Admin
+    # def get_priority_display(self, obj):
+    #     return obj.get_priority_display()  # Devuelve el valor legible del ChoiceField
 
-    def get_status_approval_display(self, obj):
-        return obj.get_status_approval_display()
-    get_status_approval_display.short_description = "Status Approval"
+    # get_priority_display.short_description = (
+    #     "Priority"  # Nombre de la columna en Django Admin
+    # )
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "title",
+                    "description",
+                    ("status_approval", "priority"),
+                    ("active", "department"),
+                    ("user", "is_main"),
+                ),
+            },
+        ),
+        (
+            "Metadata",
+            {
+                "fields": (("created", "modified"),),
+            },
+        ),
+    )
+
+    readonly_fields = ["created", "modified"]
