58d115c84d789892143a1c23c1a920798861812d
mrandersoncordero <acorderofigueroa7@gmail.com>
2025-02-13 10:29:32 -0400

    [FEAT] company model

--- apps/petitions/models/__init__.py
+++ apps/petitions/models/__init__.py
@@ -1,3 +1,4 @@
 from .petition_model import Petition
 from .petition_attachment_model import PetitionsAttachment
 from .department_model import Department
+from .company_model import Company
---
+++ apps/petitions/models/company_model.py
@@ -0,0 +1,16 @@
+"""Company model."""
+
+# Django
+from django.db import models
+
+# Utilities
+from utils.main_model import MainModel
+
+
+class Company(MainModel, models.Model):
+    """Model Company."""
+
+    name = models.CharField(max_length=100, unique=True, verbose_name="nombre")
+
+    def __str__(self) -> str:
+        return str(self.name)
