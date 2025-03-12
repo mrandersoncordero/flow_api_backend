f2875979d6d5615fd7931adaf3bb6a83cc4fc37a
mrandersoncordero <acorderofigueroa7@gmail.com>
2025-03-12 13:14:41 -0400

    [FIX] signals

--- apps/petitions/apps.py
+++ apps/petitions/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PetitionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'petitions'
+
+    def ready(self):
+        import petitions.signals
