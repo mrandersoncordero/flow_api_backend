31f5ec69dcfa3d2011585de46e763b18c3b0d373
mrandersoncordero <acorderofigueroa7@gmail.com>
2025-09-05 16:33:08 -0400

    refactor: reorganize URL patterns and improve debug toolbar integration

--- flow/urls.py
+++ flow/urls.py
@@ -3,10 +3,26 @@
 # Django
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+
+from account.views import DashBoardView
 
 urlpatterns = [
     # Django Admin
     path("admin/", admin.site.urls),
     # API v1
     path("api/v1/", include("api.urls", namespace="api")),
+    # Web App
+    path("account/", include("apps.account.urls", namespace="account")),
+    # Dashboard
+    path("", DashBoardView.as_view(), name="dashboard"),
+    # Petitions
+    path("", include("petitions.urls", namespace="petitions")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
